2215cd3ef1e48c1e7d4998aded3a5be8bd300805
rishiraj526 <rishiraj00346@gmail.com>
2025-08-04 22:03:18 +0530

    added form

--- app.js
+++ app.js
@@ -16,6 +16,13 @@ app.listen(8000,(req, res) => {
 
 
 app.get('/', (req, res) => {
- 
    res.render("index");
+});
+
+app.get('/login', (req, res) => {
+   res.render("login");
+});
+
+app.get('/register', (req, res) => {  
+   res.render("register");
 });
