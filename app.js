107cf6ff096160f1a5a37b85405db4b4d3a967a4
rishiraj526 <rishiraj00346@gmail.com>
2025-08-04 20:47:40 +0530

    added nav and footer template

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const path = require("path");
+const ejsMate = require('ejs-mate');
 
 app.use(express.static(path.join(__dirname, 'public')));    
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.engine('ejs', ejsMate);
 
 app.listen(8000,(req, res) => {
    console.log(`Server is running on port ${8000}`);
